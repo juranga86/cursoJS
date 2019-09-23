57cfad9cf7184476511fd7bf4feb24998592855d
juranga86 <54905878+juranga86@users.noreply.github.com>
2019-09-22 21:45:20 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -5,11 +5,16 @@ function mostrar()
 	while (contador < 11) {
 	
 	console.log("cont:" + contador);
-	/*si queremos hacer un corte en el contador. 
+	//si queremos hacer un corte en el contador. 
 	if(contador == 4){
-		break; // ver como funciona el: "continue;""
+		
+		break; 
+		// Lo que hace esto es que cuando la variable "contador" llega a 4 => sale de la estructura del 
+		//While, a través del "break" => va a contar: 1-2-3-4 y sale. 
+		//Por otro lado, el "continue;" lo que hace es evaluar la condición del While una vez más, para 
+		//luego salir de la estructura.
 	}
-	*/
+	
 	contador++;//sirve para incrementar el contador
 
 	
